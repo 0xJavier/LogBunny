27d04f7c37a533926c6a02ab52baefa123775dce
Javier Munoz <0xJMunoz@gmail.com>
2022-08-22 13:03:29 -0400

    add main logging

--- Sources/LogBunny/LogBunny.swift
+++ Sources/LogBunny/LogBunny.swift
@@ -1,6 +1,47 @@
-public struct LogBunny {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+public enum Log {
+    fileprivate enum LogLevel {
+        case warning
+        case error
+        
+        fileprivate var prefix: String {
+            switch self {
+            case .warning: return "WARNING"
+            case .error: return "ERROR"
+            }
+        }
+    }
+    
+    fileprivate struct Context {
+        let file: String
+        let function: String
+        let line: Int
+        var description: String {
+            return "\((file as NSString).lastPathComponent):\(line) \(function)"
+        }
+    }
+    
+    public static func warning(_ str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .warning, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    public static func error(_ str: StaticString, shouldLogContext: Bool = true, file: String = #file, function: String = #function, line: Int = #line) {
+        let context = Context(file: file, function: function, line: line)
+        Log.handleLog(level: .error, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+    
+    fileprivate static func handleLog(level: LogLevel, str: String, shouldLogContext: Bool, context: Context) {
+        let logComponents = ["[\(level.prefix)]", str]
+        
+        var fullString = logComponents.joined(separator: " ")
+        if shouldLogContext {
+            fullString += " -> \(context.description)"
+        }
+        
+        #if DEBUG
+        print(fullString)
+        #endif
     }
 }
